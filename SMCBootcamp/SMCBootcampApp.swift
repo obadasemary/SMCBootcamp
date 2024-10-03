f954b6c0eeb950ec1e9b2d8d2906cba2ad7cc6d5
Abdelrahman Mohamed <obada.semary@gmail.com>
2024-10-03 23:45:41 +0300

    Initial Commit

---
+++ SMCBootcamp/SMCBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SMCBootcampApp.swift
+//  SMCBootcamp
+//
+//  Created by Abdelrahman Mohamed on 03/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SMCBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

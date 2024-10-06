037a1d3c44b7de8ea5547a53883c18e1f50fecce
Abdelrahman Mohamed <obada.semary@gmail.com>
2024-10-06 23:56:53 +0300

    feat: introduce `DownloadmageAsync`

---
+++ SMCBootcamp/DownloadmageAsync.swift
@@ -0,0 +1,18 @@
+//
+//  DownloadmageAsync.swift
+//  SMCBootcamp
+//
+//  Created by Abdelrahman Mohamed on 06/10/2024.
+//
+
+import SwiftUI
+
+struct DownloadmageAsync: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DownloadmageAsync()
+}

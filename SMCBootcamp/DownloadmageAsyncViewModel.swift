fb9088c6989440dad9f2bfdcf56ad6c109318d24
Abdelrahman Mohamed <obada.semary@gmail.com>
2024-10-07 01:07:57 +0300

    feat: implement `DownloadmageAsyncViewModel`

--- SMCBootcamp/DownloadmageAsyncViewModel.swift
+++ SMCBootcamp/DownloadmageAsyncViewModel.swift
@@ -6,12 +6,33 @@
 //
 
 import SwiftUI
+import Combine
 
 class DownloadmageAsyncViewModel: ObservableObject {
     
-    @Published var image: Image? = nil
+    @Published var image: UIImage? = nil
+    let loader = DownloadmageAsyncImageLoader()
+    var cancellables = Set<AnyCancellable>()
     
     func fetchImage() {
-        self.image = Image(systemName: "gear.fill")
+        
+        loader.downloadwithEscaping { [weak self] image, error in
+            if let image = image {
+                DispatchQueue.main.async {
+                    self?.image = image
+                }
+            }
+        }
+    }
+    
+    func fetchImageWtihCombine() {
+        loader.downloadWithCombine()
+            .receive(on: DispatchQueue.main)
+            .sink { _ in
+                
+            } receiveValue: { [weak self] image in
+                self?.image = image
+            }
+            .store(in: &cancellables)
     }
 }

134dbf39eb56b0e332a90d2fd252f24758b4bc97
Abdelrahman Mohamed <obada.semary@gmail.com>
2024-10-06 23:55:14 +0300

    feat: introduce `DoCatchTryThrows`

---
+++ SMCBootcamp/DoCatchTryThrows.swift
@@ -0,0 +1,18 @@
+//
+//  DoCatchTryThrows.swift
+//  SMCBootcamp
+//
+//  Created by Abdelrahman Mohamed on 03/10/2024.
+//
+
+import SwiftUI
+
+struct DoCatchTryThrows: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    DoCatchTryThrows()
+}

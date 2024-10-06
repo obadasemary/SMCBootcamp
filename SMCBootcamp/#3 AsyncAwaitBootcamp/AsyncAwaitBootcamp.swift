bbf3279ffafed301d3f4f9beaf2065eeed1a5286
Abdelrahman Mohamed <obada.semary@gmail.com>
2024-10-07 01:32:10 +0300

    feat: introduce `AsyncAwaitBootcamp`

---
+++ SMCBootcamp/#3 AsyncAwaitBootcamp/AsyncAwaitBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  AsyncAwaitBootcamp.swift
+//  SMCBootcamp
+//
+//  Created by Abdelrahman Mohamed on 07/10/2024.
+//
+
+import SwiftUI
+
+struct AsyncAwaitBootcamp: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    AsyncAwaitBootcamp()
+}

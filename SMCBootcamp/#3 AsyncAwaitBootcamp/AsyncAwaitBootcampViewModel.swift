60212bff25f6de0d4f531ae3f49e422dc07adf31
Abdelrahman Mohamed <obada.semary@gmail.com>
2024-10-10 21:54:11 +0300

    feat: implement `AsyncAwaitBootcampViewModel`

--- SMCBootcamp/#3 AsyncAwaitBootcamp/AsyncAwaitBootcampViewModel.swift
+++ SMCBootcamp/#3 AsyncAwaitBootcamp/AsyncAwaitBootcampViewModel.swift
@@ -1,10 +1,32 @@
+//
+//  AsyncAwaitBootcampViewModel.swift
+//  SMCBootcamp
+//
+//  Created by Abdelrahman Mohamed on 10/10/2024.
+//
+
+
 import SwiftUI
 
 class AsyncAwaitBootcampViewModel: ObservableObject {
     
     @Published var dataArray: [String] = []
     
-    func addTitle1() {
-        dataArray.append("Title1: \(Thread.current)")
+    func addTitleMainAsyncAfter() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) { [weak self] in
+            self?.dataArray.append("Title main.asyncAfter: \(Thread.current)")
+        }
+    }
+    
+    func addTitleGlobalAsyncAfter() {
+        DispatchQueue.global().asyncAfter(deadline: .now() + 2) { [weak self] in
+            let title  = "Title global.asyncAfter: \(Thread.current)"
+            DispatchQueue.main.async {
+                self?.dataArray.append(title)
+                
+                let titleMain  = "Title main.asyncAfter: \(Thread.current)"
+                self?.dataArray.append(titleMain)
+            }
+        }
     }
 }
